84f61d9a5ffc48b16b033d689d1b661cd691468e
jacobhsu <jacob.hsu.tw@gmail.com>
2020-06-03 14:57:15 +0800

    https://cristijora.github.io/vue-tabs/#/

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueTabs from 'vue-nav-tabs'
+import 'vue-nav-tabs/themes/vue-tabs.css'
+
+Vue.use(VueTabs)
 
 Vue.config.productionTip = false
 

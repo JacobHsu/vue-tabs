9cd3e093c85e8b90fa03d880b06be4368ff7c172
jacobhsu <jacob.hsu.tw@gmail.com>
2020-06-03 15:20:16 +0800

    iframe

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # vue-tabs
 
+## References
+
+[Vue组件中如何引入iframe？](https://segmentfault.com/a/1190000016258735)
+[如何裁剪Iframe的內容以顯示頁面的一部分？](http://hk.uwenku.com/question/p-kzpuiwap-pa.html)
+
 ## Project setup
 ```
 yarn install
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,9 @@
     <vue-tabs>
         <v-tab title="First tab">
           First tab content
+          <div class="act-form">
+              <iframe :src="src" class="iframe-position"></iframe>
+          </div>
         </v-tab>
 
         <v-tab title="Second tab">
@@ -21,12 +24,26 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      src: 'https://segmentfault.com/a/1190000016258735'
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.act-form {
+  overflow: hidden;
+}
+.iframe-position {
+  width: 900px;
+  height: 700px;
+  position: relative; 
+  top: -310px;
+}
 h3 {
   margin: 40px 0 0;
 }
